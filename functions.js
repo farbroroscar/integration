4f5e91c216724e24193fa520c2328915a5909415
Oscar Lindberg <oscarlindberg@me.com>
2023-06-06 15:57:35 +0200

    fix issue with not all data from file was returned, add validation for order of in which peopleelements are in

--- functions.js
+++ functions.js
@@ -17,23 +17,35 @@ const PERSON = {
 
 const getPeople = () => {
   const peopleFromFile = fs.readFileSync("./people.txt", "utf8");
-  // todo: validate data
+
   const peopleFormated = peopleFromFile
-    .split(/[\n,]+/) // Remove linebreaks.
-    .map((line) => line.split("|")) // Divide each elment propery insted of separating them with "|".
-    .map((line) => {
-      const [_, ...rest] = line;
-      return [peopleElements[line[0]], ...rest]; // Map single charachter to word.
-    });
+    .split(/[\n,]+/) // Removes linebreaks.
+    .map((line) => line.split("|")); // Divides each elment propery insted of separating them with "|".
+
+  const validOrderOfPeopleElements = validatePeople(peopleFormated);
+
+  if (!validOrderOfPeopleElements) {
+    // fix proper message...
+    return "bad content";
+  }
+
+  const peopleCharactersMappedToWords = peopleFormated.map((element) => {
+    const [_, ...rest] = element;
+    return [peopleElements[element[0]], ...rest]; // Map single charachter to word.
+  });
 
   const separatePersons = [];
 
-  peopleFormated.reduce((acc, current, index, array) => {
+  peopleCharactersMappedToWords.reduce((acc, current, index, array) => {
     const isNewPerson = current[0] === PERSON.PERSON;
-    const isNotFirstRound = index > 1;
+    const isNotFirstRound = index > 0;
     const isLastRound = index === array.length - 1;
 
-    if ((isNewPerson && isNotFirstRound) || isLastRound) {
+    if (isLastRound) {
+      separatePersons.push([...acc, current]);
+      return;
+    }
+    if (isNewPerson && isNotFirstRound) {
       separatePersons.push(acc);
       return [current];
     }
@@ -109,4 +121,42 @@ const makeChildren = (parent, children) => {
   }
 };
 
+const validatePeople = (people) => {
+  const parentElementCharacters = people.map((tag) => tag[0]);
+  const isValidSequence = validateTagOrder(parentElementCharacters);
+  return isValidSequence;
+};
+
+const validateTagOrder = (peopleTagSequence) => {
+  const validCharachterOrder = !!peopleTagSequence.reduce(
+    (acc, currentChar) => {
+      if (!acc) {
+        return false;
+      }
+
+      const previousCharachter = acc.slice(-1)[0];
+      const canFollowP =
+        currentChar === "T" || currentChar === "A" || currentChar === "F";
+      const canFollowF =
+        currentChar === "T" || currentChar === "A" || currentChar === "P";
+
+      if (previousCharachter === "P") {
+        return canFollowP ? [...acc, currentChar] : false;
+      }
+
+      if (previousCharachter === "F") {
+        return canFollowF ? [...acc, currentChar] : false;
+      }
+      return [...acc, currentChar];
+    },
+    []
+  );
+
+  return validCharachterOrder;
+};
+
 module.exports = getPeople;
+
+// cad har jag gjort?
+// lagt till fix för att jag inte alltid fick med sista elementet.
+// lagt till validering
--- server.js
+++ server.js
@@ -5,14 +5,9 @@ const app = express();
 
 const PORT = 4000;
 
-// middleware
-// app.use((req, res, next) => {
-//   next();
-// });
-
 app.use(express.text()); // map incomming reqest body as text.
 
-app.get("/people", (req, res) => {
+app.get("/people", (_req, res) => {
   const peopleAsXML = getPeople();
   res.send(peopleAsXML);
 });
